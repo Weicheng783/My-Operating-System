a93d501b95a188b81d6009b228ced3d3cb5246cd
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-07-11 01:22:26 +0100

    Global Descriptor Table & Ports

--- kernel.cpp
+++ kernel.cpp
@@ -5,8 +5,39 @@
 
 void printf(char* str){
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x=0, y=0;
+
     for(int i=0; str[i] != '\0'; ++i){
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+        switch(str[i]){
+            case '\n':
+                y++;
+                x = 0;
+                break;
+
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80){
+            y++;
+            x = 0;
+        }
+
+        if(y >= 25){
+            for(y=0; y<25; y++){
+                for(x=0; x<80; x++){
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+                }
+            }
+
+            x = 0;
+            y = 0;
+        }
+
     }
 }
 
@@ -22,7 +53,8 @@ extern "C" void callConstructors(){
 
 
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
-    printf("Hello World! This is Weicheng's Operating System !!! ---");
+    printf("Hello World! This is Weicheng's Operating System !!! ---\n");
+    printf("Hello World! This is Weicheng's Operating System !!! ---\n");
 
     GlobalDescriptorTable gdt;
 
